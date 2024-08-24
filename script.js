447339fd80e6ae1edab9ee84e1c3f0e310dd7953
Cleitinho69 <ezequielsantos2018@outlook.com.br>
2024-08-23 21:29:13 -0300

    .

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-import * as THREE from 'three';
-import WebGL from 'three/addons/capabilities/WebGL.js';
-
-// aqui eu crio uma cena para que objetos possão ser rederizados
-const scene = new THREE.Scene();
-// aqui eu crio uma camera para que possa ver o objeto(o angulo da camera, o tamanho dela, e onde ela começa, até onde ela vai);
-const camera = new THREE.PerspectiveCamera( 75,window.innerWidth/window.innerHeight, 0.1, 1000);
-// Serve para rederizar a cena e exibir ela
+import * as THREE from '/three.js-master/src/three.js';
+// Create a scene
+const scane = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
-// serve para controlar a resulução do objeto
-// nesse caso eu to pegamdo a resolução completa da tela
-renderer.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
 
-// aqui serve para criar uma forma de um cubo
-const geometry = new THREE.BoxGeometry(0.2,3,1);
-// aqui a gente modifica a cor do objeto
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-// aqui a gente junta os dois em um objeto que possa ser rederizado
-const cube = new THREE.Mesh(geometry,material);
-// e aqui adicionamos o objeto a cena
-scene.add(cube);
-// aqui a gente deixa a camera um pouco longe para que ela não fique colada no objeto
+scane.add( cube );
+
 camera.position.z = 5;
 
-// aqui a gente inicia a animação do objeto
-function animate(){
-    renderer.render(scene,camera);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+function animate() {
+	renderer.render( scane, camera );
 }
-renderer.setAnimationLoop(animate);
+renderer.setAnimationLoop( animate );
