b8059350e3ee01f5eb601528591856fa22757dfb
Cleitinho69 <ezequielsantos2018@outlook.com.br>
2024-08-23 21:41:21 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'src/three.js';
+import * as THREE from '/src/three.js';
 // Create a scene
 const scane = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
